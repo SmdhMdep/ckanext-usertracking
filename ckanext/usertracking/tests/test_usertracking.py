02d1627196cc26d2c49a09631501d6a5124a786f
D-Simona-G <120456967+D-Simona-G@users.noreply.github.com>
2022-12-21 12:20:52 +0000

    Update test_usertracking.py

--- ckanext/usertracking/tests/test_usertracking.py
+++ ckanext/usertracking/tests/test_usertracking.py
@@ -23,16 +23,16 @@ class TestUsertrackingPlugin(object):
         pass
     
     def test_page_engagement_tracking(self):
-        rows, count = UserTracking.page_engagement_tracking('','24')
+        rows, count = UserTrackingDAO.page_engagement_tracking('','24')
         # TODO: Modify assert to contain relevant dummy data
         assert (rows, count) == ([], 0)
 
     def test_org_engagement_tracking(self):
-        user_page_access = UserTracking.org_engagement_tracking('','24')
+        user_page_access = UserTrackingDAO.org_engagement_tracking('','24')
         # TODO: Modify assert to contain relevant dummy data
         assert user_page_access == ([], 0)
 
     def test_user_engagement_tracking(self):
-        user_activity = UserTracking.user_engagement_tracking('','1')
+        user_activity = UserTrackingDAO.user_engagement_tracking('','1')
         # TODO: Modify assert to contain relevant dummy data
         assert user_activity == ([], 0)
